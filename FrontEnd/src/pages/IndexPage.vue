3a7ea44ef1d87f39e0e2f386ea2a4865c385141e
nannaryy <117610863+nannary@users.noreply.github.com>
2023-04-04 15:47:29 +0700

    Infinit Scroll Post In indexPage

--- FrontEnd/src/pages/IndexPage.vue
+++ FrontEnd/src/pages/IndexPage.vue
@@ -7,7 +7,6 @@
             font-size: 25px;
             font-weight: bolder;
             margin-bottom: -10px;
-
             color: #880e4f;
           "
         >
@@ -49,206 +48,124 @@
           </q-list>
         </q-btn-dropdown>
 
-        <section class="post">
-          <router-link
-            to="/postncomment"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >? ทำไมนรกต้องใช้กระทะทองแดง</router-link
-          >
-          <br />
-          <Content style="color: #5c6bc0"
-            >ทำไมนรกต้องใช้กระทะทองแดง ทำไมไม่ใช้กระทะเงิน
-            ซึ่งนำความร้อนได้ดีกว่า</Content
-          ><br />
-          <ion-avatar>
-            <img src="/public/pf1.png" style="width: 30px; height: 30px" />
-          </ion-avatar>
-          &nbsp;
-          <router-link
-            to="/myprofile"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >User0001</router-link
-          >
-          &nbsp;
-          <!-- ส่วนของไลก์และคอมเมนต์ -->
-          <p style="display: inline; color: #5c6bc0">3 มีนาคม 2566</p>
-          <i
-            class="fa-solid fa-heart"
-            style="color: #880e4f; margin-left: 420px"
-          ></i>
-          <i style="color: #880e4f; margin-right: 10px">24</i>
-          <i class="fa-solid fa-comment" style="color: #880e4f"></i>
-          <i style="color: #880e4f; margin-right: 10px">20</i>
-
-          <hr style="width: 700px" />
-        </section>
-
         <!-- ----------------------------------------------------------------------------------------------------------- -->
+        <div class="q-pa-md">
+          <!-- <q-infinite-scroll :distance="1" :disable="currentPage >= totalPage"> -->
+          <q-scroll-area style="height: 380px">
+            <!-- Post -->
+            <!-- loop ข้อมูลโพสต์ -->
+            <section
+              class="post"
+              v-for="(item, index) in postList"
+              :key="index"
+            >
+              <router-link
+                :to="'/postncomment/' + item.id"
+                style="
+                  text-decoration: none;
+                  color: black;
+                  font-weight: bolder;
+                  color: #1a237e;
+                "
+                >? {{ item.id }} {{ item.title }}
+              </router-link>
+              <br />
+              <Content style="color: #5c6bc0"> {{ item.content }} </Content
+              ><br />
+              <ion-avatar>
+                <img :src="item.picture.x" style="width: 30px; height: 30px" />
+              </ion-avatar>
+              &nbsp;
+              <router-link
+                :to="'/myprofile/' + item.user_id"
+                style="
+                  text-decoration: none;
+                  color: black;
+                  font-weight: bolder;
+                  color: #1a237e;
+                "
+              >
+                {{ item.username }}
+              </router-link>
+              &nbsp;
+              <!-- ส่วนของไลก์และคอมเมนต์ -->
+              <p style="display: inline; color: #5c6bc0">
+                {{ item.create_date }}
+              </p>
+              <i
+                class="fa-solid fa-heart"
+                style="color: #880e4f; margin-left: 370px"
+              ></i>
+              <i style="color: #880e4f; margin-right: 5px">
+                {{ item.like_count }}
+              </i>
+              <i class="fa-solid fa-comment" style="color: #880e4f"></i>
+              <i style="color: #880e4f; margin-right: 5px">
+                {{ item.comment_count }}
+              </i>
+              <hr style="width: 700px" />
+            </section>
+            <!-- post -->
 
-        <section class="post">
-          <router-link
-            to="/postncomment"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >? ต้นมะนาวบนหลุมศพหมาที่โดนงูเห่ากัด</router-link
-          >
-          <br />
-          <Content style="color: #5c6bc0"
-            >ปลูกต้นมะนาวไว้บนหลุมศพหมาที่โดนงูเห่ากัด กินได้ไหมคะ
-            มีอันตรายจากพิษงูไหม</Content
-          ><br />
-          <ion-avatar>
-            <img src="/public/pf2.png" style="width: 30px; height: 30px" />
-          </ion-avatar>
-          &nbsp;
-          <router-link
-            to="/myprofile"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >User0002</router-link
-          >
-          &nbsp;
-          <p style="display: inline; color: #5c6bc0">9 พฤษภาคม 2566</p>
-
-          <!-- ส่วนของไลก์และคอมเมนต์ -->
-          <i
-            class="fa-solid fa-heart"
-            style="color: #880e4f; margin-left: 420px"
-          ></i>
-          <i style="color: #880e4f; margin-right: 10px">24</i>
-          <i class="fa-solid fa-comment" style="color: #880e4f"></i>
-          <i style="color: #880e4f; margin-right: 10px">20</i>
-          <hr style="width: 700px" />
-        </section>
-
-        <!-- ----------------------------------------------------------------------------------------------------------- -->
-
-        <section class="post">
-          <router-link
-            to="/postncomment"
-            class="post"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >?
-            พ่อขุนรามนี่ถือว่าได้เป็นอัจฉริยะทางภาษาหรือเปล่าครับ</router-link
-          >
-          <br />
-          <Content style="color: #5c6bc0"
-            >หลักการสร้างตัวอักษรและการสะกดคำ ท่านได้แนวคิดมาจากไหน
-            มีเหตุจำเป็นอะไรที่ต้องสร้างอักษรไทย</Content
-          ><br />
-          <ion-avatar>
-            <img src="/public/pf3.png" style="width: 30px; height: 30px" />
-          </ion-avatar>
-          &nbsp;
-          <router-link
-            to="/myprofile"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >User0003</router-link
-          >
-          &nbsp;
-          <p style="display: inline; color: #5c6bc0">17 สิงหาคม 2566</p>
-
-          <!-- ส่วนของไลก์และคอมเมนต์ -->
-          <i
-            class="fa-solid fa-heart"
-            style="color: #880e4f; margin-left: 420px"
-          ></i>
-          <i style="color: #880e4f; margin-right: 10px">24</i>
-          <i class="fa-solid fa-comment" style="color: #880e4f"></i>
-          <i style="color: #880e4f; margin-right: 10px">20</i>
-          <hr style="width: 700px" />
-        </section>
-
-        <!-- ----------------------------------------------------------------------------------------------------------- -->
-        <section class="post">
-          <router-link
-            to="/postncomment"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >แบ่งปัน 7 ไอเทมเด็ดที่ควรมีติดกระเป๋า!</router-link
-          >
-          <br />
-          <Content style="color: #5c6bc0"
-            >สวัสดีชาวบลูทุกท่านค่ะ
-            วันนี้เราจะมาแบ่งปันไอเทมที่มีติดกระเป๋าแล้วชีวิตง่ายขึ้นแบบ
-            200%</Content
-          ><br />
-          <ion-avatar>
-            <img src="/public/pf4.png" style="width: 30px; height: 30px" />
-          </ion-avatar>
-          &nbsp;
-          <router-link
-            to="/myprofile"
-            style="
-              text-decoration: none;
-              color: black;
-              font-weight: bolder;
-              color: #1a237e;
-            "
-            >User0004</router-link
-          >
-          &nbsp;
-          <p style="display: inline; color: #5c6bc0">24 ธันวาคม 2566</p>
-          <!-- ส่วนของไลก์และคอมเมนต์ -->
-          <i
-            class="fa-solid fa-heart"
-            style="color: #880e4f; margin-left: 420px"
-          ></i>
-          <i style="color: #880e4f; margin-right: 10px">24</i>
-          <i class="fa-solid fa-comment" style="color: #880e4f"></i>
-          <i style="color: #880e4f; margin-right: 10px">20</i>
-        </section>
-
+            <!-- loading -->
+            <div
+              class="row justify-center q-my-md"
+              v-if="postList.length"
+              v-intersection="handleScrolledToBottom"
+            >
+              <q-spinner-dots
+                v-if="loding === false"
+                color="primary"
+                size="40px"
+              />
+            </div>
+          </q-scroll-area>
+          <!-- loading -->
+          <!-- </q-infinite-scroll> -->
+        </div>
         <!-- ----------------------------------------------------------------------------------------------------------- -->
       </div>
+      <!-- <p>Page : {{ currentPage }}</p> -->
+      <!-- เลขหน้า -->
+      <!-- <div class="q-gutter-md" style="display: flex; justify-content: center">
+        <q-pagination
+          v-model="currentPage"
+          :max="totalPage"
+          direction-links
+          flat
+          color="grey"
+          active-color="pink"
+        />
+      </div> -->
+      <!-- เลขหน้า -->
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineAsyncComponent, defineComponent, ref } from "vue";
+import { ref, onMounted, onUnmounted, watch } from "vue";
 import { fabYoutube } from "@quasar/extras/fontawesome-v6";
 import { biTranslate, biGlobe, biCheck } from "@quasar/extras/bootstrap-icons";
 import { useLang } from "src/composables/useLang";
 
-const MyChart = defineAsyncComponent(() => import("components/MyChart.vue"));
+import { useAxios } from "src/composables/useAxios";
+// เรียกใช้ Post API
+import { PostApi } from "src/api/PostApi";
 
 export default {
   name: "MyLayout",
-  components: {},
   setup() {
+    const { getPostList } = PostApi();
+
+    // ตัวแปรแสดงข้อมูลโพสต์
+    const postList = ref([]);
+    // เลขหน้าเพจ
+    const currentPage = ref(1);
+    // จำนวนรายการ
+    const recordPerPage = ref(3);
+    const totalPage = ref(0);
+    const loding = ref(false);
+
     const { localeList, t, locale } = useLang();
     const leftDrawerOpen = ref(false);
     const search = ref("");
@@ -256,9 +173,51 @@ export default {
     function toggleLeftDrawer() {
       leftDrawerOpen.value = !leftDrawerOpen.value;
     }
+    // แสดง List Post
+    onMounted(async () => {
+      fetchList();
+    });
+
+    const fetchList = async () => {
+      const response = await getPostList({
+        page: currentPage.value, //method: "GET",
+        perPage: recordPerPage.value,
+      });
+      if (response) {
+        // postList.value = response.dataList;
+        postList.value.push(...response.dataList);
+        totalPage.value = response.appPagination;
+        console.log(response);
+      }
+    };
+
+    // watch(currentPage, async (newVal, oldVal) => {
+    //   fetchList();
+    //   console.log("CurrentPage change from", newVal);
+    // });
+    // เมื่อเลื่อน Scroll ลงมา
+    const handleScrolledToBottom = (isVisible) => {
+      setTimeout(() => {
+        loding.value = true;
+        if (!isVisible) {
+          return;
+        }
+        if (currentPage.value >= totalPage.value) {
+          return;
+        }
+        // เพิ่มหน้ารายการ ทีละ 2 รายการ
+        currentPage.value++;
+        recordPerPage.value = 2;
+        fetchList();
+        // ถ้าหมดแล้วจะ timelog
+        console.log("timeout");
+        loding.value = false;
+        // มีการรอ loading
+      }, 2000);
+    };
+
     return {
       localeList,
-      MyChart,
       t,
       locale,
       biGlobe,
@@ -267,13 +226,20 @@ export default {
       fabYoutube,
       leftDrawerOpen,
       search,
+      // return รายการข้อมูล
+      currentPage,
+      totalPage,
+      // fetchList,
       toggleLeftDrawer,
+      handleScrolledToBottom,
+      loding,
       links1: [
         { icon: "home", text: "Home", link: "/" },
         { icon: "whatshot", text: "List Page", link: "/list-page" },
         { icon: "subscriptions", text: "Subscriptions" },
         { icon: biTranslate, text: "Translate", link: "/locale-page" },
       ],
+      postList,
     };
   },
 };
@@ -298,5 +264,7 @@ export default {
   padding: 10px;
   background: white;
   justify-items: space-around;
+  margin-top: 50px; /* เว้นระยะห่างด้านบน */
+  margin-bottom: 50px; /* เว้นระยะห่างด้านล่าง */
 }
 </style>
